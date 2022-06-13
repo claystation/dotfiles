1853a201f2fb1e3712dd29f95686598dc52f57e3
claystation <c.wijnekus@gmail.com>
2022-06-13 14:03:54 +0200

    Inital install scripts

---
+++ run_once_10_install_packages.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Packages
+sudo pacman -Syu neovim \
+	thunderbird \
+	firefox \
+	unzip \
+	rofi \
+    	go
+
+# AURs
+pamac build slack-desktop \
+	spotify \
+    	polybar
+
+# VIM Plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
