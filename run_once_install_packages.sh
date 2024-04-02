d2f32a307b355bd86f16ba8577b97c54a5c3f68d
Claystation <c.wijnekus@gmail.com>
2024-04-02 15:42:10 +0200

    Install packages script

---
+++ run_once_install_packages.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Packages
+sudo pacman -Syu neovim \
+	firefox \
+	unzip \
+	rofi \
+	polybar
+
+e# Change Shell
+if [ $SHELL != $(which zsh) ]; then
+    chsh -s $(which zsh)
+fi
+
+# Install Oh My Zsh
+# https://ohmyz.sh/#install
+if ! [ -d ~/.oh-my-zsh ]; then
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+# Nerd Fonts
+mkdir -p ~/.local/share/fonts
+
+if ! [ -d ~/.local/share/fonts/Hack ]; then
+    wget -O ~/Downloads/Hack.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Hack.zip
+    unzip ~/Downloads/Hack.zip -d ~/.local/share/fonts/Hack
+    rm ~/Downloads/Hack.zip
+fi
+
+fc-cache -fv
+
+# asdf VM
+if ! [ -d ~/.local ]; then
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+fi
+
+asdf plugin add golang
+asdf install golang latest
